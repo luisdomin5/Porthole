ac5ef99f2b3d0cbd2c2274f3546c39955879d671
mcthomas <mcthomas4@wisc.edu>
2020-08-22 21:58:51 -0500

    Removed nonsense

--- og.py
+++ og.py
@@ -3,7 +3,7 @@ import re
 
 params = {
   'access_key': '459e61878d08703f89f0a077e85fef87',
-  'url': 'https://www.cnn.com/2020/08/22/politics/what-matters-august-21/index.html'
+  'url': 'https://www.npr.org/2020/08/22/905099950/the-worst-is-not-behind-us-california-continues-to-burn'
 }
 
 api_result = requests.get('http://api.scrapestack.com/scrape', params)
--- test.py
+++
@@ -1,2 +0,0 @@
-egg = ""
-print len(egg)
